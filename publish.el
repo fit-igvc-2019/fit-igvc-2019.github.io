4208378ea7dde01bcc917bc3505bc2d4e83e2b54
Viet Dung Nguyen <dnguyen98@protonmail.com>
2019-09-05 00:24:26 -0400

    Write publish script

---
+++ publish.el
@@ -0,0 +1,38 @@
+(require 'ox)
+(require 'ox-publish)
+(require 'ox-beamer)
+(require 'ox-latex)
+(require 'ox-html)
+
+(defvar pdf-dir
+  (concat default-directory "pdf/"))
+
+(defvar presentation-dir
+  (concat default-directory "presentation/"))
+
+(defvar html-dir
+  (concat default-directory "html/"))
+
+(setq
+ org-publish-project-alist
+ `(("pdf"
+    :base-directory ,pdf-dir
+    :base-extension "org"
+    :publishing-directory ,pdf-dir
+    :publishing-function org-latex-publish-to-pdf
+    )
+   ("presentation"
+    :base-directory ,presentation-dir
+    :base-extension "org"
+    :publishing-directory ,presentation-dir
+    :publishing-function org-beamer-publish-to-pdf
+    )
+   ("html"
+    :base-directory ,html-dir
+    :base-extension "org"
+    :publishing-directory ,default-directory
+    :publishing-function org-html-publish-to-html
+    )
+   ))
+
+(org-publish-all)
