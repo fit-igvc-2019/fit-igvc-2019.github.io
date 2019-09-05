603e7d84bb670da411cce1cbe96e5cb2095c47c4
Viet Dung Nguyen <dnguyen98@protonmail.com>
2019-09-05 18:24:36 -0400

    Split org files, cleanup files after export

---
+++ cleanup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+find org -type  f -not -iname "*org" -exec rm {} \;
--- publish.el
+++ publish.el
@@ -4,14 +4,8 @@
 (require 'ox-latex)
 (require 'ox-html)
 
-(defvar pdf-dir
-  (concat default-directory "pdf/"))
-
-(defvar presentation-dir
-  (concat default-directory "presentation/"))
-
-(defvar html-dir
-  (concat default-directory "html/"))
+(defvar org-dir
+  (concat default-directory "org/"))
 
 (setq
  org-publish-timestamp-directory
@@ -26,23 +20,24 @@
 (setq
  org-publish-project-alist
  `(("pdf"
-    :base-directory ,pdf-dir
+    :base-directory ,(concat org-dir "pdf")
     :base-extension "org"
-    :publishing-directory ,pdf-dir
+    :publishing-directory ,(concat default-directory "pdf")
     :publishing-function org-latex-publish-to-pdf
     )
    ("presentation"
-    :base-directory ,presentation-dir
+    :base-directory ,(concat org-dir "presentation")
     :base-extension "org"
-    :publishing-directory ,presentation-dir
+    :publishing-directory ,(concat default-directory "presentation")
     :publishing-function org-beamer-publish-to-pdf
     )
    ("html"
-    :base-directory ,html-dir
+    :base-directory ,(concat org-dir "html")
     :base-extension "org"
     :publishing-directory ,default-directory
     :publishing-function org-html-publish-to-html
-    )
-   ))
+    )))
 
 (org-publish-all)
+
+(shell-command (concat "bash " default-directory "cleanup.sh"))
